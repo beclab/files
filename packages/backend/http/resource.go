907b6f3707261c5c99093c6adb39ae7bc38fa07f
lovehunter9 <wangrx07@aliyun.com>
2024-10-17 21:15:50 +0800

    google drive path to id - first step

--- packages/backend/http/resource.go
+++ packages/backend/http/resource.go
@@ -137,8 +137,8 @@ func resourceGetGoogle(w http.ResponseWriter, r *http.Request, stream int) (int,
 	fmt.Println("src Path:", src)
 	//src = strings.Trim(src, "/") + "/"
 
-	origin := r.Header.Get("Origin")
-	fmt.Println("*****resourceGetGoogle origin: ", origin)
+	host := r.Host
+	fmt.Println("*****resourceGetGoogle host: ", host)
 	pathId, srcDrive, srcName, err := GoogleDrivePathToId(src, w, r)
 	if err != nil {
 		return errToStatus(err), err
@@ -214,8 +214,8 @@ var resourceGetHandler = withUser(func(w http.ResponseWriter, r *http.Request, d
 	if srcType == "sync" {
 		return resourceGetSync(w, r, stream)
 	} else if srcType == "google" {
-		origin := r.Header.Get("Origin")
-		fmt.Println("*****resourceGetHandler origin: ", origin)
+		host := r.Host
+		fmt.Println("*****resourceGetHandler host: ", host)
 		return resourceGetGoogle(w, r, stream)
 	}
 
