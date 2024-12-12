49e60a080254e981401af7ffbe7eca8737108d8d
lovehunter9 <wangrx07@aliyun.com>
2024-12-12 20:31:50 +0800

    upload/download json for clouds

--- packages/backend/http/awss3.go
+++ packages/backend/http/awss3.go
@@ -237,8 +237,8 @@ type Awss3TaskData struct {
 	DriverName    string               `json:"driver_name"`
 	FailedReason  *string              `json:"failed_reason,omitempty"`
 	WorkerName    *string              `json:"worker_name,omitempty"`
-	CreatedAt     *time.Time           `json:"created_at,omitempty"`
-	UpdatedAt     *time.Time           `json:"updated_at,omitempty"`
+	CreatedAt     *string              `json:"created_at,omitempty"`
+	UpdatedAt     *string              `json:"updated_at,omitempty"`
 }
 
 type Awss3TaskResponse struct {
