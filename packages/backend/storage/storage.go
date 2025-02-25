e9dd0e7a065ca3601dec0ba44981ea108ee0e37d
lovehunter9 <wangrx07@aliyun.com>
2025-02-25 20:59:55 +0800

    style: delete share relative

--- packages/backend/storage/storage.go
+++ packages/backend/storage/storage.go
@@ -3,7 +3,6 @@ package storage
 import (
 	"github.com/filebrowser/filebrowser/v2/auth"
 	"github.com/filebrowser/filebrowser/v2/settings"
-	"github.com/filebrowser/filebrowser/v2/share"
 	"github.com/filebrowser/filebrowser/v2/users"
 )
 
@@ -11,7 +10,6 @@ import (
 // verifications when fetching and saving data to ensure consistency.
 type Storage struct {
 	Users    users.Store
-	Share    *share.Storage
 	Auth     *auth.Storage
 	Settings *settings.Storage
 }
