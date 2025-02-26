dd8350ac84b5835864ee9921d7db666bab16ee7d
lovehunter9 <wangrx07@aliyun.com>
2025-02-26 17:53:41 +0800

    style: delete all unused cmds

--- packages/backend/cmd/utils.go
+++ packages/backend/cmd/utils.go
@@ -1,18 +1,12 @@
 package cmd
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
+	"github.com/asdine/storm/v3"
+	"github.com/spf13/cobra"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
-
-	"github.com/asdine/storm/v3"
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	yaml "gopkg.in/yaml.v2"
 
 	"github.com/filebrowser/filebrowser/v2/settings"
 	"github.com/filebrowser/filebrowser/v2/storage"
@@ -25,23 +19,23 @@ func checkErr(err error) {
 	}
 }
 
-func mustGetString(flags *pflag.FlagSet, flag string) string {
-	s, err := flags.GetString(flag)
-	checkErr(err)
-	return s
-}
-
-func mustGetBool(flags *pflag.FlagSet, flag string) bool {
-	b, err := flags.GetBool(flag)
-	checkErr(err)
-	return b
-}
-
-func mustGetUint(flags *pflag.FlagSet, flag string) uint {
-	b, err := flags.GetUint(flag)
-	checkErr(err)
-	return b
-}
+//func mustGetString(flags *pflag.FlagSet, flag string) string {
+//	s, err := flags.GetString(flag)
+//	checkErr(err)
+//	return s
+//}
+//
+//func mustGetBool(flags *pflag.FlagSet, flag string) bool {
+//	b, err := flags.GetBool(flag)
+//	checkErr(err)
+//	return b
+//}
+//
+//func mustGetUint(flags *pflag.FlagSet, flag string) uint {
+//	b, err := flags.GetUint(flag)
+//	checkErr(err)
+//	return b
+//}
 
 func generateKey() []byte {
 	k, err := settings.GenerateKey()
@@ -107,51 +101,51 @@ func python(fn pythonFunc, cfg pythonConfig) cobraFunc {
 	}
 }
 
-func marshal(filename string, data interface{}) error {
-	fd, err := os.Create(filename)
-	checkErr(err)
-	defer fd.Close()
-
-	switch ext := filepath.Ext(filename); ext {
-	case ".json":
-		encoder := json.NewEncoder(fd)
-		encoder.SetIndent("", "    ")
-		return encoder.Encode(data)
-	case ".yml", ".yaml": //nolint:goconst
-		encoder := yaml.NewEncoder(fd)
-		return encoder.Encode(data)
-	default:
-		return errors.New("invalid format: " + ext)
-	}
-}
-
-func unmarshal(filename string, data interface{}) error {
-	fd, err := os.Open(filename)
-	checkErr(err)
-	defer fd.Close()
-
-	switch ext := filepath.Ext(filename); ext {
-	case ".json":
-		return json.NewDecoder(fd).Decode(data)
-	case ".yml", ".yaml":
-		return yaml.NewDecoder(fd).Decode(data)
-	default:
-		return errors.New("invalid format: " + ext)
-	}
-}
-
-func jsonYamlArg(cmd *cobra.Command, args []string) error {
-	if err := cobra.ExactArgs(1)(cmd, args); err != nil {
-		return err
-	}
-
-	switch ext := filepath.Ext(args[0]); ext {
-	case ".json", ".yml", ".yaml":
-		return nil
-	default:
-		return errors.New("invalid format: " + ext)
-	}
-}
+//func marshal(filename string, data interface{}) error {
+//	fd, err := os.Create(filename)
+//	checkErr(err)
+//	defer fd.Close()
+//
+//	switch ext := filepath.Ext(filename); ext {
+//	case ".json":
+//		encoder := json.NewEncoder(fd)
+//		encoder.SetIndent("", "    ")
+//		return encoder.Encode(data)
+//	case ".yml", ".yaml": //nolint:goconst
+//		encoder := yaml.NewEncoder(fd)
+//		return encoder.Encode(data)
+//	default:
+//		return errors.New("invalid format: " + ext)
+//	}
+//}
+//
+//func unmarshal(filename string, data interface{}) error {
+//	fd, err := os.Open(filename)
+//	checkErr(err)
+//	defer fd.Close()
+//
+//	switch ext := filepath.Ext(filename); ext {
+//	case ".json":
+//		return json.NewDecoder(fd).Decode(data)
+//	case ".yml", ".yaml":
+//		return yaml.NewDecoder(fd).Decode(data)
+//	default:
+//		return errors.New("invalid format: " + ext)
+//	}
+//}
+//
+//func jsonYamlArg(cmd *cobra.Command, args []string) error {
+//	if err := cobra.ExactArgs(1)(cmd, args); err != nil {
+//		return err
+//	}
+//
+//	switch ext := filepath.Ext(args[0]); ext {
+//	case ".json", ".yml", ".yaml":
+//		return nil
+//	default:
+//		return errors.New("invalid format: " + ext)
+//	}
+//}
 
 func cleanUpInterfaceMap(in map[interface{}]interface{}) map[string]interface{} {
 	result := make(map[string]interface{})
@@ -183,11 +177,11 @@ func cleanUpMapValue(v interface{}) interface{} {
 // convertCmdStrToCmdArray checks if cmd string is blank (whitespace included)
 // then returns empty string array, else returns the splitted word array of cmd.
 // This is to ensure the result will never be []string{""}
-func convertCmdStrToCmdArray(cmd string) []string {
-	var cmdArray []string
-	trimmedCmdStr := strings.TrimSpace(cmd)
-	if trimmedCmdStr != "" {
-		cmdArray = strings.Split(trimmedCmdStr, " ")
-	}
-	return cmdArray
-}
+//func convertCmdStrToCmdArray(cmd string) []string {
+//	var cmdArray []string
+//	trimmedCmdStr := strings.TrimSpace(cmd)
+//	if trimmedCmdStr != "" {
+//		cmdArray = strings.Split(trimmedCmdStr, " ")
+//	}
+//	return cmdArray
+//}
