fd723bdc1a13b3b7bb31da6ab22518390d148689
lovehunter9 <wangrx07@aliyun.com>
2025-03-27 20:02:38 +0800

    feat: path uri relative

--- pkg/http/raw.go
+++ pkg/http/raw.go
@@ -7,7 +7,12 @@ import (
 )
 
 func rawHandler(w http.ResponseWriter, r *http.Request, d *common.Data) (int, error) {
-	srcType := r.URL.Query().Get("src")
+	//srcType := r.URL.Query().Get("src")
+	srcType, err := drives.ParsePathType(r.URL.Path, r, false, true)
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
 	handler, err := drives.GetResourceService(srcType)
 	if err != nil {
 		return http.StatusBadRequest, err
