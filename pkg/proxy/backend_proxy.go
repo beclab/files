380c8912fb5308bd713f7eabcfd687661d0c2008
lovehunter9 <wangrx07@aliyun.com>
2025-04-03 15:05:08 +0800

    use src/src_type/dst_type no more: paste rewrite bug

--- pkg/proxy/backend_proxy.go
+++ pkg/proxy/backend_proxy.go
@@ -223,6 +223,9 @@ func rewriteUrl(path string, pvc string, prefix string) string {
 		splitIndex, splitName := minWithNegativeOne(homeIndex, applicationIndex, "/Home", "/Application")
 		if splitIndex != -1 {
 			firstHalf := path[:splitIndex]
+			if firstHalf == "" {
+				firstHalf = "/"
+			}
 			secondHalf := path[splitIndex:]
 			klog.Info("firstHalf=", firstHalf)
 			klog.Info("secondHalf=", secondHalf)
