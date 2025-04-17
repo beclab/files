af225dec72508aea38f85028480321e9fcc40dc6
lovehunter9 <wangrx07@aliyun.com>
2025-04-17 20:11:09 +0800

    copy progress: drive, same, copy test

--- pkg/fileutils/copy.go
+++ pkg/fileutils/copy.go
@@ -3,12 +3,11 @@ package fileutils
 import (
 	"files/pkg/pool"
 	"fmt"
+	"github.com/spf13/afero"
 	"k8s.io/klog/v2"
 	"os"
 	"path"
-	"time"
-
-	"github.com/spf13/afero"
+	"sync"
 )
 
 // Copy copies a file or folder from one place to another.
@@ -47,29 +46,41 @@ func Copy(fs afero.Fs, task *pool.Task, src, dst string) error {
 		return err
 	}
 
-	go task.UpdateProgressFromRsync(progressChan)
-
-	// 模拟外部获取进度
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	done := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
-		time.Sleep(10 * time.Second) // 模拟一些其他操作
-		done <- true
+		defer wg.Done()
+		task.UpdateProgressFromRsync(progressChan)
 	}()
 
-	for {
-		select {
-		case <-ticker.C:
-			if storedTask, ok := pool.TaskManager.Load(task.ID); ok {
-				if t, ok := storedTask.(*pool.Task); ok {
-					fmt.Printf("Task %s Progress: %d%%\n", t.ID, t.GetProgress())
-				}
-			}
-		case <-done:
-			fmt.Println("Operation completed or stopped.")
-			return nil
-		}
-	}
+	// 等待 goroutine 完成（如果需要同步行为）
+	// 如果不需要同步，可以省略 wg.Wait()，但要注意 goroutine 泄漏问题
+	wg.Wait()
+
+	return nil
 }
+
+//// 模拟外部获取进度
+//ticker := time.NewTicker(1 * time.Second)
+//defer ticker.Stop()
+//
+//done := make(chan bool)
+//go func() {
+//	time.Sleep(10 * time.Second) // 模拟一些其他操作
+//	done <- true
+//}()
+//
+//for {
+//	select {
+//	case <-ticker.C:
+//		if storedTask, ok := pool.TaskManager.Load(task.ID); ok {
+//			if t, ok := storedTask.(*pool.Task); ok {
+//				fmt.Printf("Task %s Progress: %d%%\n", t.ID, t.GetProgress())
+//			}
+//		}
+//	case <-done:
+//		fmt.Println("Operation completed or stopped.")
+//		return nil
+//	}
+//}
+//}
