8c8ec8803c7822a337eca205f3ada2054d6332da
lovehunter9 <wangrx07@aliyun.com>
2025-04-22 11:29:09 +0800

    copy progress: drive, same, copy test

--- pkg/fileutils/rsync.go
+++ pkg/fileutils/rsync.go
@@ -14,16 +14,15 @@ import (
 )
 
 func ExecuteRsyncSimulated(source, dest string) (chan int, error) {
-	progressChan := make(chan int, 10) // 使用缓冲通道
+	progressChan := make(chan int, 10)
 
 	go func() {
 		defer close(progressChan)
 
-		// 模拟进度从 0 到 100，每秒增加 1
 		for i := 0; i <= 100; i++ {
 			klog.Infof("Send progress: %d", i)
-			progressChan <- i           // 发送进度到通道
-			time.Sleep(1 * time.Second) // 模拟每秒更新一次
+			progressChan <- i
+			time.Sleep(1 * time.Second)
 		}
 	}()
 
@@ -32,10 +31,11 @@ func ExecuteRsyncSimulated(source, dest string) (chan int, error) {
 
 func ExecuteRsync(source, dest string) (chan int, error) {
 	progressChan := make(chan int, 100)
-	bwLimit := 1024
+	//bwLimit := 1024
 
 	stdoutReader, stdoutWriter := io.Pipe()
-	cmd := exec.Command("rsync", "-av", "--info=progress2", fmt.Sprintf("--bwlimit=%d", bwLimit), source, dest)
+	//cmd := exec.Command("rsync", "-av", "--info=progress2", fmt.Sprintf("--bwlimit=%d", bwLimit), source, dest)
+	cmd := exec.Command("rsync", "-av", "--info=progress2", source, dest)
 	cmd.Stdout = stdoutWriter
 
 	go func() {
@@ -64,14 +64,12 @@ func ExecuteRsync(source, dest string) (chan int, error) {
 				lines := strings.Split(output, "\n")
 				for i, line := range lines {
 					if line != "" {
-						// 处理行尾回车符
 						if i == len(lines)-1 && !strings.HasSuffix(line, "\n") {
 							line = strings.TrimSuffix(line, "\r")
 						}
 
 						var matched bool
 
-						// 尝试提取百分比
 						matches := re.FindAllStringSubmatch(line, -1)
 						if len(matches) > 0 {
 							for _, match := range matches {
@@ -85,10 +83,8 @@ func ExecuteRsync(source, dest string) (chan int, error) {
 							}
 						}
 
-						// 未匹配到百分比时的处理
 						if !matched {
-							fmt.Printf("未匹配到百分比: %s\n", line)
-							klog.Infof("未匹配到百分比: %s", line)
+							klog.Infof("No percent info in: %s", line)
 						}
 					}
 				}
@@ -112,7 +108,6 @@ func ExecuteRsync(source, dest string) (chan int, error) {
 	return progressChan, nil
 }
 
-// 辅助函数：将字符串转换为float64，处理可能的格式问题
 func parseFloat(s string) float64 {
 	f, err := strconv.ParseFloat(s, 64)
 	if err != nil {
