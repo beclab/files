e368d89b29509cc8c1393a2533c4c6765e577e54
caixiangyue <caixiangyue007@gmail.com>
2025-05-19 16:36:24 +0800

    test

--- pkg/http/raw.go
+++ pkg/http/raw.go
@@ -4,6 +4,8 @@ import (
 	"files/pkg/common"
 	"files/pkg/drives"
 	"net/http"
+
+	"k8s.io/klog/v2"
 )
 
 func rawHandler(w http.ResponseWriter, r *http.Request, d *common.Data) (int, error) {
@@ -12,6 +14,8 @@ func rawHandler(w http.ResponseWriter, r *http.Request, d *common.Data) (int, er
 	if err != nil {
 		return http.StatusBadRequest, err
 	}
+	klog.Info("rawHandler", "srcType", srcType, "handler", handler)
 
+	klog.Info(w, r, d)
 	return handler.RawHandler(w, r, d)
 }
