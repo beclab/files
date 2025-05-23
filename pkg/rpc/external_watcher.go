98c8f62deb1825e0c039758f18130a2a700c66ef
lovehunter9 <wangrx07@aliyun.com>
2025-05-23 22:30:18 +0800

    basic test

--- pkg/rpc/external_watcher.go
+++ pkg/rpc/external_watcher.go
@@ -2,10 +2,7 @@ package rpc
 
 import (
 	"bytetrade.io/web3os/fs-lib/jfsnotify"
-	"files/pkg/drives"
 	"k8s.io/klog/v2"
-	"sync"
-	"time"
 )
 
 var externalWatcher *jfsnotify.Watcher = nil
@@ -20,23 +17,6 @@ func InitExternalWatcher() {
 		}
 		klog.Infoln("~~~Debug Log: externalWatcher created")
 	}
-
-	//err = filepath.Walk("/data", func(path string, info fs.FileInfo, err error) error {
-	//	if err != nil {
-	//		return err
-	//	}
-	//	if info.IsDir() {
-	//		klog.Infoln("filepath.Base: ", filepath.Base(path))
-	//		if filepath.Base(path) == strings.Trim(files.ExternalPrefix, "/") {
-	//			err = externalWatcher.Add(path)
-	//			if err != nil {
-	//				klog.Errorln("watcher add error:", err)
-	//				return err
-	//			}
-	//		}
-	//	}
-	//	return nil
-	//})
 	path := "/External"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -44,7 +24,6 @@ func InitExternalWatcher() {
 		panic(err)
 	}
 	klog.Infof("watcher initialized at %s", path)
-
 	path = "/data/External"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -52,7 +31,6 @@ func InitExternalWatcher() {
 		panic(err)
 	}
 	klog.Infof("watcher initialized at %s", path)
-
 	path = "/wangrongxiang"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -60,7 +38,6 @@ func InitExternalWatcher() {
 		panic(err)
 	}
 	klog.Infof("watcher initialized at %s", path)
-
 	path = "/data/wangrongxiang"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -68,7 +45,6 @@ func InitExternalWatcher() {
 		panic(err)
 	}
 	klog.Infof("watcher initialized at %s", path)
-
 	path = "/Application"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -76,7 +52,6 @@ func InitExternalWatcher() {
 		panic(err)
 	}
 	klog.Infof("watcher initialized at %s", path)
-
 	path = "/data/Application"
 	err = externalWatcher.Add(path)
 	if err != nil {
@@ -86,124 +61,221 @@ func InitExternalWatcher() {
 	klog.Infof("watcher initialized at %s", path)
 
 	// Start listening for events.
-	go dedupExternalLoop(externalWatcher)
-
-	//path := strings.TrimSuffix(RootPrefix+files.ExternalPrefix, "/")
-	//klog.Infof("~~~Debug Log: Watching external files: %s", path)
-	//err = externalWatcher.Add(path)
-	//if err != nil {
-	//	klog.Errorln("watcher add error:", err)
-	//	panic(err)
-	//}
-	//klog.Infoln("~~~Debug Log: externalWatcher initialized with path:", path)
-}
-
-func dedupExternalLoop(w *jfsnotify.Watcher) {
-	var (
-		waitFor      = 1000 * time.Millisecond
-		mu           sync.Mutex
-		timers       = make(map[string]*time.Timer)
-		pendingEvent = make(map[string]jfsnotify.Event)
-		printEvent   = func(e jfsnotify.Event) {
-			klog.Infof("handle event %v %v", e.Op.String(), e.Name)
-		}
-	)
-	klog.Infof("~~~Debug Log: dedup watcher started with %v", w)
-
 	go func() {
+		klog.Infof("~~~Debug Log: dedup watcher started")
 		for {
-			mu.Lock()
-			toProcess := make(map[string]*time.Timer)
-			for name, t := range timers {
-				toProcess[name] = t
-			}
-			mu.Unlock()
-
-			for name, t := range toProcess {
-				select {
-				case <-t.C:
-					mu.Lock()
-					if ev, ok := pendingEvent[name]; ok {
-						delete(pendingEvent, name)
-						delete(timers, name)
-						mu.Unlock()
-
-						printEvent(ev)
-						err := handleExternalEvent(ev)
-						if err != nil {
-							klog.Errorf("handle watch file event error %s", err.Error())
-						}
-					} else {
-						mu.Unlock()
-					}
-				case <-time.After(waitFor):
-					continue
+			klog.Infof("~~~Debug Log: event for")
+			select {
+			case err, ok := <-externalWatcher.Errors:
+				if !ok {
+					printTime("ERROR: %s", err)
+					return
 				}
+			case e, ok := <-externalWatcher.Events:
+				if !ok {
+					klog.Warning("watcher event channel closed")
+					return
+				}
+				klog.Infof("external event %s", e.Name)
 			}
-			time.Sleep(100 * time.Millisecond)
 		}
 	}()
-
-	klog.Infof("~~~Debug Log: dedup watcher started")
-	for {
-		klog.Infof("~~~Debug Log: event for")
-		select {
-		case err, ok := <-w.Errors:
-			if !ok {
-				return
-			}
-			printTime("ERROR: %s", err)
-
-		case e, ok := <-w.Events:
-			if !ok {
-				klog.Warning("watcher event channel closed")
-				return
-			}
-
-			if e.Has(jfsnotify.Chmod) {
-				continue
-			}
-
-			//if strings.HasSuffix(filepath.Dir(e.Name), "/.uploadstemp") {
-			//	continue
-			//}
-
-			mu.Lock()
-			pendingEvent[e.Name] = e
-			t, ok := timers[e.Name]
-			if !ok {
-				t = time.NewTimer(waitFor)
-				timers[e.Name] = t
-			} else {
-				t.Reset(waitFor)
-			}
-			mu.Unlock()
-		}
-	}
 }
 
-func handleExternalEvent(e jfsnotify.Event) error {
-	klog.Infof("~~~Debug Log: handle event %v %v", e.Op.String(), e.Name)
-	//if strings.HasSuffix(filepath.Dir(e.Name), "/.uploadstemp") {
-	//	//klog.Infoln("we won't deal with uploads temp dir")
-	//	return nil
-	//}
-
-	if e.Has(jfsnotify.Remove) || e.Has(jfsnotify.Rename) {
-		klog.Infof("external delete %s", e.Name)
-		drives.GetMountedData(nil)
-		//next line must be commented for rename
-		//return nil
-	}
-
-	if e.Has(jfsnotify.Create) {
-		klog.Infof("external create %s", e.Name)
-		drives.GetMountedData(nil)
-		return nil
-	}
-
-	if e.Has(jfsnotify.Write) {
-		klog.Infof("external write %s", e.Name)
-	}
-	return nil
-}
+//func InitExternalWatcher() {
+//	var err error
+//	if externalWatcher == nil {
+//		externalWatcher, err = jfsnotify.NewWatcher("externalWatcher")
+//		if err != nil {
+//			klog.Fatalf("Failed to initialize watcher: %v", err)
+//			panic(err)
+//		}
+//		klog.Infoln("~~~Debug Log: externalWatcher created")
+//	}
+//
+//	//err = filepath.Walk("/data", func(path string, info fs.FileInfo, err error) error {
+//	//	if err != nil {
+//	//		return err
+//	//	}
+//	//	if info.IsDir() {
+//	//		klog.Infoln("filepath.Base: ", filepath.Base(path))
+//	//		if filepath.Base(path) == strings.Trim(files.ExternalPrefix, "/") {
+//	//			err = externalWatcher.Add(path)
+//	//			if err != nil {
+//	//				klog.Errorln("watcher add error:", err)
+//	//				return err
+//	//			}
+//	//		}
+//	//	}
+//	//	return nil
+//	//})
+//	path := "/External"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	path = "/data/External"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	path = "/wangrongxiang"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	path = "/data/wangrongxiang"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	path = "/Application"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	path = "/data/Application"
+//	err = externalWatcher.Add(path)
+//	if err != nil {
+//		klog.Errorln("watcher add error:", err)
+//		panic(err)
+//	}
+//	klog.Infof("watcher initialized at %s", path)
+//
+//	// Start listening for events.
+//	go dedupExternalLoop(externalWatcher)
+//
+//	//path := strings.TrimSuffix(RootPrefix+files.ExternalPrefix, "/")
+//	//klog.Infof("~~~Debug Log: Watching external files: %s", path)
+//	//err = externalWatcher.Add(path)
+//	//if err != nil {
+//	//	klog.Errorln("watcher add error:", err)
+//	//	panic(err)
+//	//}
+//	//klog.Infoln("~~~Debug Log: externalWatcher initialized with path:", path)
+//}
+//
+//func dedupExternalLoop(w *jfsnotify.Watcher) {
+//	var (
+//		waitFor      = 1000 * time.Millisecond
+//		mu           sync.Mutex
+//		timers       = make(map[string]*time.Timer)
+//		pendingEvent = make(map[string]jfsnotify.Event)
+//		printEvent   = func(e jfsnotify.Event) {
+//			klog.Infof("handle event %v %v", e.Op.String(), e.Name)
+//		}
+//	)
+//	klog.Infof("~~~Debug Log: dedup watcher started with %v", w)
+//
+//	go func() {
+//		for {
+//			mu.Lock()
+//			toProcess := make(map[string]*time.Timer)
+//			for name, t := range timers {
+//				toProcess[name] = t
+//			}
+//			mu.Unlock()
+//
+//			for name, t := range toProcess {
+//				select {
+//				case <-t.C:
+//					mu.Lock()
+//					if ev, ok := pendingEvent[name]; ok {
+//						delete(pendingEvent, name)
+//						delete(timers, name)
+//						mu.Unlock()
+//
+//						printEvent(ev)
+//						err := handleExternalEvent(ev)
+//						if err != nil {
+//							klog.Errorf("handle watch file event error %s", err.Error())
+//						}
+//					} else {
+//						mu.Unlock()
+//					}
+//				case <-time.After(waitFor):
+//					continue
+//				}
+//			}
+//			time.Sleep(100 * time.Millisecond)
+//		}
+//	}()
+//
+//	klog.Infof("~~~Debug Log: dedup watcher started")
+//	for {
+//		klog.Infof("~~~Debug Log: event for")
+//		select {
+//		case err, ok := <-w.Errors:
+//			if !ok {
+//				return
+//			}
+//			printTime("ERROR: %s", err)
+//
+//		case e, ok := <-w.Events:
+//			if !ok {
+//				klog.Warning("watcher event channel closed")
+//				return
+//			}
+//
+//			if e.Has(jfsnotify.Chmod) {
+//				continue
+//			}
+//
+//			//if strings.HasSuffix(filepath.Dir(e.Name), "/.uploadstemp") {
+//			//	continue
+//			//}
+//
+//			mu.Lock()
+//			pendingEvent[e.Name] = e
+//			t, ok := timers[e.Name]
+//			if !ok {
+//				t = time.NewTimer(waitFor)
+//				timers[e.Name] = t
+//			} else {
+//				t.Reset(waitFor)
+//			}
+//			mu.Unlock()
+//		}
+//	}
+//}
+//
+//func handleExternalEvent(e jfsnotify.Event) error {
+//	klog.Infof("~~~Debug Log: handle event %v %v", e.Op.String(), e.Name)
+//	//if strings.HasSuffix(filepath.Dir(e.Name), "/.uploadstemp") {
+//	//	//klog.Infoln("we won't deal with uploads temp dir")
+//	//	return nil
+//	//}
+//
+//	if e.Has(jfsnotify.Remove) || e.Has(jfsnotify.Rename) {
+//		klog.Infof("external delete %s", e.Name)
+//		drives.GetMountedData(nil)
+//		//next line must be commented for rename
+//		//return nil
+//	}
+//
+//	if e.Has(jfsnotify.Create) {
+//		klog.Infof("external create %s", e.Name)
+//		drives.GetMountedData(nil)
+//		return nil
+//	}
+//
+//	if e.Has(jfsnotify.Write) {
+//		klog.Infof("external write %s", e.Name)
+//	}
+//	return nil
+//}
