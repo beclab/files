c5ee5b4582769fd671bfb763076756f523070b36
lovehunter9 <wangrx07@aliyun.com>
2025-05-23 22:03:45 +0800

    debug

--- pkg/rpc/watcher.go
+++ pkg/rpc/watcher.go
@@ -145,10 +145,6 @@ func WatchPath(addPaths []string, deletePaths []string, focusPaths []string) {
 			}
 			panic(err)
 		}
-
-		// Start listening for events.
-		go dedupLoop(watcher)
-		klog.Infof("watching path %s", strings.Join(addPaths, ","))
 	}
 
 	currentTime := time.Now().Format(time.RFC3339)
@@ -262,6 +258,10 @@ func WatchPath(addPaths []string, deletePaths []string, focusPaths []string) {
 	}
 
 	klog.Infoln("Finished watching path setup.")
+
+	// Start listening for events.
+	go dedupLoop(watcher)
+	klog.Infof("watching path %s", strings.Join(addPaths, ","))
 }
 
 func dedupLoop(w *jfsnotify.Watcher) {
