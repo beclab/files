b2df466d51025cb4f14f411b40d43e8686f2be61
lovehunter9 <wangrx07@aliyun.com>
2025-08-25 17:48:28 +0800

    fix form bug

--- pkg/redisutils/redis_client.go
+++ pkg/redisutils/redis_client.go
@@ -1,6 +1,12 @@
 package redisutils
 
 import (
+	//"context"
+	//"github.com/cloudwego/hertz/pkg/app/client"
+	//"github.com/cloudwego/hertz/pkg/app/middlewares/client/sd"
+	//"github.com/cloudwego/hertz/pkg/common/config"
+	//"github.com/cloudwego/hertz/pkg/common/hlog"
+	//"github.com/hertz-contrib/registry/redis"
 	"github.com/go-redis/redis"
 	"os"
 	"strconv"
@@ -32,4 +38,17 @@ func InitRedis() {
 		Password: redisPassword,               // "difyai123456",
 		DB:       redisDB,                     //0,
 	})
+	//cli, err := client.NewClient()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//r := redis.NewRedisResolver(redisHost+":"+redisPort, redis.WithPassword(redisPassword), redis.WithDB(redisDB))
+	//cli.Use(sd.Discovery(r))
+	//for i := 0; i < 10; i++ {
+	//	status, body, err := cli.Get(context.Background(), nil, "http://hertz.test.demo/ping", config.WithSD(true))
+	//	if err != nil {
+	//		hlog.Fatal(err)
+	//	}
+	//	hlog.Infof("HERTZ: code=%d,body=%s", status, string(body))
+	//}
 }
