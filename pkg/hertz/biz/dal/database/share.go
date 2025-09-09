39fa14e1a9b405f6dfe60234b8dab81928dc2d5b
lovehunter9 <wangrx07@aliyun.com>
2025-09-09 11:51:15 +0800

    feat: add share relative to 1.12.2

---
+++ pkg/hertz/biz/dal/database/share.go
@@ -0,0 +1,143 @@
+package database
+
+import (
+	"files/pkg/common"
+	"files/pkg/hertz/biz/model/api/share"
+	"fmt"
+	"k8s.io/klog/v2"
+	"strconv"
+	"time"
+)
+
+// share_path
+
+func CreateSharePath(paths []*share.SharePath) ([]*share.SharePath, error) {
+	result := DB.Create(paths)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return paths, nil
+}
+
+func DeleteSharePath(pathID string) error {
+	return DB.Where("id = ?", pathID).Delete(&share.SharePath{}).Error
+}
+
+func UpdateSharePath(pathID string, updates map[string]interface{}) error {
+	if updates["update_time"] == nil {
+		updates["update_time"] = time.Now().UTC().Format(time.RFC3339Nano)
+	}
+	return DB.Model(&share.SharePath{}).
+		Where("id = ?", pathID).
+		Updates(updates).Error
+}
+
+func QuerySharePath(params *QueryParams, page, pageSize int64, orderBy, order string, joinParams []*JoinCondition) ([]*share.SharePath, int64, error) {
+	var res []*share.SharePath
+	total, err := QueryData(&share.SharePath{}, &res, params, page, pageSize, orderBy, order, joinParams)
+	if err != nil {
+		klog.Error(err)
+		return nil, 0, err
+	}
+	return res, total, nil
+}
+
+func VerifySharePathPassword(pathID, inputPassword string) (bool, error) {
+	var path share.SharePath
+	if err := DB.Select("password_md5").Where("id = ?", pathID).First(&path).Error; err != nil {
+		return false, err
+	}
+	return path.PasswordMd5 == common.Md5String(inputPassword), nil
+}
+
+func CheckSharePathExpired(pathID string) (bool, error) {
+	var path share.SharePath
+	if err := DB.Select("expire_time").Where("id = ?", pathID).First(&path).Error; err != nil {
+		return false, err
+	}
+
+	if path.ExpireTime == "" {
+		return false, nil
+	}
+
+	var expireTime time.Time
+	if _, err := strconv.ParseInt(path.ExpireTime, 10, 64); err == nil {
+		millis, _ := strconv.ParseInt(path.ExpireTime, 10, 64)
+		expireTime = time.UnixMilli(millis).UTC()
+	} else {
+		var err error
+		expireTime, err = time.Parse(time.RFC3339, path.ExpireTime)
+		if err != nil {
+			return false, fmt.Errorf("invalid expire_time format: %s", path.ExpireTime)
+		}
+	}
+
+	now := time.Now().UTC()
+	return now.After(expireTime), nil
+}
+
+// share_token
+
+func CreateShareToken(tokens []*share.ShareToken) ([]*share.ShareToken, error) {
+	result := DB.Create(tokens)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return tokens, nil
+}
+
+func DeleteShareToken(token string) error {
+	return DB.Where("token = ?", token).Delete(&share.ShareToken{}).Error
+}
+
+func UpdateShareToken(tokenID int64, updates map[string]interface{}) error {
+	return DB.Model(&share.ShareToken{}).
+		Where("id = ?", tokenID).
+		Updates(updates).Error
+}
+
+func QueryShareToken(params *QueryParams, page, pageSize int64, orderBy, order string, joinParams []*JoinCondition) ([]*share.ShareToken, int64, error) {
+	var res []*share.ShareToken
+	total, err := QueryData(&share.ShareToken{}, &res, params, page, pageSize, orderBy, order, joinParams)
+	if err != nil {
+		klog.Error(err)
+		return nil, 0, err
+	}
+	return res, total, nil
+}
+
+// share member
+
+func CreateShareMember(members []*share.ShareMember) ([]*share.ShareMember, error) {
+	result := DB.Create(members)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return members, nil
+}
+
+func DeleteShareMember(memberID int64) error {
+	return DB.Where("id = ?", memberID).Delete(&share.ShareMember{}).Error
+}
+
+func UpdateShareMember(memberID int64, updates map[string]interface{}) error {
+	if updates["update_time"] == nil {
+		updates["update_time"] = time.Now().UTC().Format(time.RFC3339Nano)
+	}
+	return DB.Model(&share.ShareMember{}).
+		Where("id = ?", memberID).
+		Updates(updates).Error
+}
+
+func QueryShareMember(params *QueryParams, page, pageSize int64, orderBy, order string, joinParams []*JoinCondition) ([]*share.ShareMember, int64, error) {
+	var res []*share.ShareMember
+	total, err := QueryData(&share.ShareMember{}, &res, params, page, pageSize, orderBy, order, joinParams)
+	if err != nil {
+		klog.Error(err)
+		return nil, 0, err
+	}
+	return res, total, nil
+}
