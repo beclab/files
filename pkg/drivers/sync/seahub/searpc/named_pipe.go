59d8bb33dc9343124fd4c6b056b54062b83bde16
lovehunter9 <wangrx07@aliyun.com>
2026-01-04 19:39:11 +0800

    fix: sync reconnection at pipe client when sending has met broken pipe

--- pkg/drivers/sync/seahub/searpc/named_pipe.go
+++ pkg/drivers/sync/seahub/searpc/named_pipe.go
@@ -3,8 +3,12 @@ package searpc
 import (
 	"encoding/binary"
 	"files/pkg/common"
+	"fmt"
 	"net"
+	"strings"
 	"sync"
+
+	"k8s.io/klog/v2"
 )
 
 type NamedPipeException struct {
@@ -19,6 +23,7 @@ type NamedPipeTransport struct {
 	SearpcTransport
 	socketPath string
 	conn       net.Conn
+	client     *NamedPipeClient
 }
 
 func (c *NamedPipeClient) validateTransport(t *NamedPipeTransport) bool {
@@ -26,11 +31,13 @@ func (c *NamedPipeClient) validateTransport(t *NamedPipeTransport) bool {
 }
 
 func (t *NamedPipeTransport) Connect() error {
-	conn, err := net.Dial("unix", t.socketPath)
-	if err != nil {
-		return err
+	if t.conn == nil {
+		conn, err := net.Dial("unix", t.socketPath)
+		if err != nil {
+			return err
+		}
+		t.conn = conn
 	}
-	t.conn = conn
 	return nil
 }
 
@@ -42,40 +49,78 @@ func (t *NamedPipeTransport) Stop() {
 }
 
 func (t *NamedPipeTransport) Send(service, fcallStr string) (string, error) {
-	// klog.Infof("Send called - service: %s, fcallStr: %s", service, fcallStr)
+	klog.Infof("Send called - service: %s, fcallStr: %s", service, fcallStr)
+	var respStr string
+	var err error
+
+	if respStr, err = t.trySend(service, fcallStr); err == nil {
+		return respStr, nil
+	}
+
+	if isRetryableError(err) {
+		klog.Errorf("[RPC] sending has met retryable err: %v, retry once immediately", err)
+		if respStr, err = t.trySend(service, fcallStr); err == nil {
+			return respStr, nil
+		}
+	}
+	klog.Errorf("[RPC] send error, err: %v", err)
+	return "", fmt.Errorf("sync server connection failed")
+}
+
+func (t *NamedPipeTransport) trySend(service, fcallStr string) (string, error) {
+	if err := t.Connect(); err != nil {
+		return "", err
+	}
 
 	reqBody := map[string]string{
 		"service": service,
 		"request": fcallStr,
 	}
-
 	jsonData := common.ToBytes(reqBody)
-
 	header := make([]byte, 4)
 	binary.LittleEndian.PutUint32(header, uint32(len(jsonData)))
-
 	sendData := append(header, jsonData...)
 
 	if _, err := t.conn.Write(sendData); err != nil {
+		t.handleConnectionError(err)
 		return "", err
 	}
 
 	respHeader := make([]byte, 4)
-	_, err := t.conn.Read(respHeader)
-	if err != nil {
+	if _, err := t.conn.Read(respHeader); err != nil {
+		t.handleConnectionError(err)
 		return "", err
 	}
 	respSize := binary.LittleEndian.Uint32(respHeader)
-
 	respBody := make([]byte, respSize)
-	_, err = t.conn.Read(respBody)
-	if err != nil {
+	if _, err := t.conn.Read(respBody); err != nil {
+		t.handleConnectionError(err)
 		return "", err
 	}
+	return string(respBody), nil
+}
 
-	respStr := string(respBody)
+func (t *NamedPipeTransport) handleConnectionError(connErr error) {
+	klog.Errorf("[RPC] Connection Error: %v", connErr)
+	t.Stop()
+	_, err := t.client.syncTransport(t)
+	if err != nil {
+		klog.Errorf("Failed to refresh transport: %v", err)
+	}
+}
 
-	return respStr, nil
+func isRetryableError(err error) bool {
+	retryable := []string{
+		"broken pipe",
+	}
+
+	errMsg := err.Error()
+	for _, e := range retryable {
+		if strings.Contains(errMsg, e) {
+			return true
+		}
+	}
+	return false
 }
 
 type NamedPipeClient struct {
@@ -99,21 +144,55 @@ func NewNamedPipeClient(socketPath, serviceName string, poolSize int) *NamedPipe
 func (c *NamedPipeClient) getTransport() (*NamedPipeTransport, error) {
 	select {
 	case t := <-c.pool:
-		return t, nil
-	default:
-		transport := &NamedPipeTransport{socketPath: c.socketPath}
-		if err := transport.Connect(); err != nil {
-			return nil, err
+		if t.conn != nil {
+			return t, nil
 		}
-		return transport, nil
+		return c.syncTransport(nil)
+	default:
+		return c.syncTransport(nil)
+	}
+}
+
+func (c *NamedPipeClient) syncTransport(old *NamedPipeTransport) (*NamedPipeTransport, error) {
+	klog.Infof("[RPC] Creating New Transport")
+	newT := &NamedPipeTransport{
+		socketPath: c.socketPath,
+		client:     c,
+	}
+	if err := newT.Connect(); err != nil {
+		klog.Errorf("[RPC] Failed to connect: %v", err)
+		return nil, fmt.Errorf("sync server connection failed")
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if old != nil {
+		klog.Infof("[RPC] Stop Old Transport")
+		old.Stop()
+	}
+
+	select {
+	case c.pool <- newT:
+		klog.Infof("[RPC] Transport Added to Pool")
+		return newT, nil
+	default:
+		newT.Stop()
+		klog.Infof("[RPC] Transport Rejected from Pool")
+		return nil, fmt.Errorf("connection pool full")
 	}
 }
 
 func (c *NamedPipeClient) returnTransport(t *NamedPipeTransport) {
-	select {
-	case c.pool <- t:
-	default:
-		t.Stop()
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if t.conn != nil {
+		select {
+		case c.pool <- t:
+		default:
+			t.Stop()
+		}
 	}
 }
 
@@ -123,6 +202,5 @@ func (c *NamedPipeClient) CallRemoteFuncSync(fcallStr string) (string, error) {
 		return "", err
 	}
 	defer c.returnTransport(transport)
-
 	return transport.Send(c.serviceName, fcallStr)
 }
--- pkg/hertz/idl/callback.thrift
+++ pkg/hertz/idl/callback.thrift
@@ -14,11 +14,7 @@ struct CallbackDeleteReq {
 struct CallbackDeleteResp {
 }
 
-struct CallbackConnectResp {
-}
-
 service CallbackService {
     CallbackCreateResp CallbackCreateMethod(1: CallbackCreateReq request) (api.post="/callback/create");
     CallbackDeleteResp CallbackDeleteMethod(1: CallbackDeleteReq request) (api.post="/callback/delete");
-    CallbackConnectResp CallbackConnectMethod() (api.post="/callback/connect");
 }
