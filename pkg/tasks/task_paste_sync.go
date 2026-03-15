2ee3e2d75d23e9f3a5c3411629374da2f9717752
lovehunter9 <wangrx07@aliyun.com>
2026-03-15 16:07:00 +0800

    add retry to uploads func

--- pkg/tasks/task_paste_sync.go
+++ pkg/tasks/task_paste_sync.go
@@ -772,14 +772,14 @@ func (t *Task) UploadDirToSync(src, dst *models.FileParam, root bool) error {
 			err = t.UploadDirToSync(fsrcFileParam, fdstFileParam, false)
 			if err != nil {
 				klog.Errorf("[Task] Sync upload dir error: %v, path: %s", err, dst.Path)
-				errs = append(errs, fmt.Errorf("[Task] Sync upload dir error: %v, path: %s", err, dst.Path))
+				errs = append(errs, fmt.Errorf("[Task] Sync upload dir error: %s, path: %s", err.Error(), dst.Path))
 			}
 		} else {
 			// Perform the file copy.
 			err = t.UploadFileToSync(fsrcFileParam, fdstFileParam)
 			if err != nil {
 				klog.Errorf("[Task] Sync upload file error: %v, path: %s", err, dst.Path)
-				errs = append(errs, fmt.Errorf("[Task] Sync upload dir error: %v, path: %s", err, dst.Path))
+				errs = append(errs, fmt.Errorf("[Task] Sync upload dir error: %s, path: %s", err.Error(), dst.Path))
 			}
 		}
 	}
